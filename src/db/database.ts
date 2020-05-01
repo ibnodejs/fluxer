308611b1f36b089592975f3e6bd005fe60e752e8
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-05-01 02:37:41 -0400

    db run

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,10 +1,10 @@
 import * as Influx from 'influx';
 import { marketDataSchema } from './marketdata.schema';
+import { influxDbHost, databaseName } from '../config';
 
-// TODO init from config
 const influx = new Influx.InfluxDB({
-    host: 'localhost',
-    database: 'exodus',
+    host: influxDbHost,
+    database: databaseName,
     schema: [marketDataSchema]
 })
 
