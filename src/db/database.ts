bf18d9e6b013390e8bbbbd64dfc7be1f08995be4
Ceddy Muhoza <ceddymuhoza@gmail.com>
2020-07-06 22:24:51 -0400

    implement user and password

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,14 +1,21 @@
 import * as Influx from 'influx';
 import { marketDataSchema } from './marketdata.schema';
-import { influxDbHost, influxDbPort, databaseName } from '../config';
+import { influxDbHost, influxDbPort, influxDbUser, influxDbPass, databaseName } from '../config';
 
 export type GroupBy = '10s' | '1m' | '5m' | '10m' | '30m' | '1h' | '6h' | '12h' | '1d' | '7d' | '30d' | '365d';
 
-const influx = new Influx.InfluxDB({
+const config: Influx.ISingleHostConfig = {
     port: influxDbPort,
     host: influxDbHost,
     database: databaseName,
     schema: [marketDataSchema]
-})
+};
+
+if (influxDbUser) {
+    config.username = influxDbUser;
+    config.password = influxDbPass;
+}
+
+const influx = new Influx.InfluxDB(config);
 
 export default influx;
