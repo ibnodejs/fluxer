36309ca4df75cd127f0437efa5f2eb5fc517e1ce
ceddybi <ceddymuhoza@gmail.com>
2022-10-27 14:00:59 -0400

    polygon provider

---
+++ src/providers/providers.test.ts
@@ -0,0 +1,20 @@
+import "mocha";
+
+import { PolygonProvider } from "./polygon";
+import { expect } from "chai";
+
+const polygon = new PolygonProvider();
+
+describe("Marketdata Providers tests", () => {
+  it("should get marketdata from polygon", async () => {
+    const bars = await polygon.getBars({
+      symbol: "AAPL",
+      start: new Date("2022-10-26"),
+      end: new Date("2022-10-27"),
+    });
+
+    console.log("PolygonProvider -> ", bars);
+
+    expect(bars.length).to.be.greaterThan(300);
+  });
+});
