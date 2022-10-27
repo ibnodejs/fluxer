edda517b2542f1ef29338cf6e9462bffe8c4f903
ceddybi <ceddymuhoza@gmail.com>
2022-10-27 13:32:46 -0400

    fluxer init

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,6 +3,7 @@ import "reflect-metadata";
 import { token, url } from "./config";
 
 import FluxerResolver from "./fluxer.resolver";
+import { HOSTNAME } from "src/config";
 import { InfluxDB } from "@influxdata/influxdb-client";
 import { MarketDataSchema } from "./marketdata.schema";
 import { RoadmanBuild } from "roadman";
@@ -10,27 +11,22 @@ import { Router } from "express";
 import isEmpty from "lodash/isEmpty";
 import { log } from "@roadmanjs/logs";
 import { queryMeasurement } from "./query";
+import { writeMeasurement } from "./write";
 
 export let influxDB: InfluxDB;
 
-export const fluxerRoadman = (args: RoadmanBuild): RoadmanBuild => {
-  const { app } = args;
-  // if no token, url do not enable influxDB
-  if (!token) {
-    log("fluxerRoadman cannot be enabled");
-  } else {
-    log("fluxerRoadman is enabled");
-    influxDB = new InfluxDB({ url, token, timeout: 10000 * 70 });
-
-    if (app) {
-      app.use("/fluxer", fluxerHttpQuery());
-    }
-  }
-  return args;
-};
-
 export const fluxerHttpQuery = () => {
   const router = Router();
+
+  router.get("/", function (req, res) {
+    res.json({
+      hostname: HOSTNAME,
+      date: new Date(),
+    });
+
+    log("health check");
+  });
+
   router.post("/", async function async(req, res) {
     const {
       symbol = "AAPL",
@@ -83,7 +79,96 @@ export const fluxerHttpQuery = () => {
     }
   });
 
+  // @deprecated
+  router.post("/insert", async function (req, res) {
+    const data = req && req.body;
+    const defaultTimestamp = new Date();
+    const items: MarketDataSchema[] = [];
+
+    try {
+      // If is array
+      if (Array.isArray(data)) {
+        data.map((item) => {
+          // Init with defaults
+          const {
+            symbol = "UNKNOWN",
+            open = 0,
+            high = 0,
+            low = 0,
+            close = 0,
+            volume = 0,
+            date = defaultTimestamp,
+          } = item as MarketDataSchema;
+
+          items.push({
+            open,
+            high,
+            low,
+            close,
+            volume,
+            symbol,
+            date: new Date(date),
+          });
+        });
+      } else {
+        // Single object
+        const item = data as any;
+        const {
+          symbol = "UNKNOWN",
+          open = 0,
+          high = 0,
+          low = 0,
+          close = 0,
+          volume = 0,
+          date = defaultTimestamp,
+        } = item;
+
+        if (item && item.symbol) {
+          items.push({
+            open,
+            high,
+            low,
+            close,
+            volume,
+            symbol,
+            date: new Date(date),
+          });
+        }
+      }
+
+      if (!isEmpty(items)) {
+        res.json({ status: 200 }); // non blocking
+        await writeMeasurement(items);
+        return log(`${JSON.stringify(items[0])} ---> `, items.length);
+      }
+
+      throw new Error("items are empty");
+    } catch (error) {
+      log("error inserting items into influxDB", error);
+      res.status(401);
+      res.end();
+    }
+  });
+
   return router;
+};
+
+export const fluxerRoadman = async (
+  args: RoadmanBuild
+): Promise<RoadmanBuild> => {
+  const { app } = args;
+  // if no token, url do not enable influxDB
+  if (!token) {
+    log("fluxerRoadman cannot be enabled");
+  } else {
+    log("fluxerRoadman is enabled");
+    influxDB = new InfluxDB({ url, token, timeout: 10000 * 70 });
+
+    if (app) {
+      app.use("/fluxer", fluxerHttpQuery());
+    }
+  }
+  return args;
 };
 
 export const getFluxerResolvers = () => [FluxerResolver];
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-import { runApp } from "./app";
+import "./sentry";
+
+import { PORT, appName, bucket, org, url } from "./config";
+import { log, roadman } from "roadman";
+
+import FluxerResolver from "./db/fluxer.resolver";
+import { fluxerRoadman } from "./db";
+
+export async function runApp(): Promise<boolean> {
+  try {
+    const running = await roadman({
+      roadmen: [fluxerRoadman],
+      resolvers: [FluxerResolver],
+    });
+
+    if (!running) {
+      throw new Error("Error running app");
+    }
+
+    log(`Started ${appName} on ${PORT}`);
+    log("started server with bucket", { bucket, org, url });
+
+    return true;
+  } catch (error) {
+    log("error running app", error);
+    console.error(error);
+    process.exit(1);
+  }
+}
 
 runApp();
